ad1207f779235ef52d7e2682a87ddf39ff14ac25
polina <palinagolgaya@yandex.ru>
2024-03-26 14:46:39 +0300

    nura func

--- main.py
+++ main.py
@@ -4,29 +4,6 @@ from telebot import types
 bot = telebot.TeleBot('7190036484:AAG1KC_QhMtZLDPopV3gW6ELKpvFlhrcvGo')
 
 
-class Nura():
-    def __init__(self, message):
-        self.message = message
-
-    def start_nura(self):
-        mupcup = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton('Помидоры', callback_data='tomatoes')
-        btn2 = types.InlineKeyboardButton('Огурцы', callback_data='cucumbers')
-        btn3 = types.InlineKeyboardButton('Перцы', callback_data='peppers')
-        btn4 = types.InlineKeyboardButton('Кабачки', callback_data='zucchini')
-        btn5 = types.InlineKeyboardButton('Морковь', callback_data='carrot')
-        btn6 = types.InlineKeyboardButton('Клубника', callback_data='strawberry')
-        btn7 = types.InlineKeyboardButton('Назад', callback_data='return')
-        mupcup.row(btn1, btn2)
-        mupcup.row(btn3, btn4)
-        mupcup.row(btn5, btn6)
-        mupcup.row(btn7)
-        bot.send_message(message.chat.id, 'Привет, внучок! \n'
-                                          'Меня зовут Баба Нюра и я знаю все о помидорках и клубнике!\n'
-                                          'Если тебе нужна моя помощь, то просто выбирай нужную культуру',
-                         reply_markup=mupcup)
-
-
 @bot.message_handler(commands=['start'])
 def start(message):
     marcup = types.ReplyKeyboardMarkup()
@@ -49,6 +26,25 @@ def on_click(message):
         pass
     elif message.text == 'Игры на выживание':
         pass
+
+
+def start_nura(message):
+    mupcup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Помидоры', callback_data='tomatoes')
+    btn2 = types.InlineKeyboardButton('Огурцы', callback_data='cucumbers')
+    btn3 = types.InlineKeyboardButton('Перцы', callback_data='peppers')
+    btn4 = types.InlineKeyboardButton('Кабачки', callback_data='zucchini')
+    btn5 = types.InlineKeyboardButton('Морковь', callback_data='carrot')
+    btn6 = types.InlineKeyboardButton('Клубника', callback_data='strawberry')
+    btn7 = types.InlineKeyboardButton('Назад', callback_data='return')
+    mupcup.row(btn1, btn2)
+    mupcup.row(btn3, btn4)
+    mupcup.row(btn5, btn6)
+    mupcup.row(btn7)
+    bot.send_message(message.chat.id, 'Привет, внучок! \n'
+                                      'Меня зовут Баба Нюра и я знаю все о помидорках и клубнике!\n'
+                                      'Если тебе нужна моя помощь, то просто выбирай нужную культуру',
+                     reply_markup=mupcup)
 
 
 bot.polling(none_stop=True)
