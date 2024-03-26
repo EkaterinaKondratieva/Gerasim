4f460185b6db93225dc134b4222a55e198ca7306
polina <palinagolgaya@yandex.ru>
2024-03-26 13:09:25 +0300

    подключили бота

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('lalalala')
+import telebot
+
+bot = telebot.TeleBot('7190036484:AAG1KC_QhMtZLDPopV3gW6ELKpvFlhrcvGo')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет!')
+
+
+bot.polling(none_stop=True)
