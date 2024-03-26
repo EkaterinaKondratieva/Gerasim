81dbe547a18dec6e2f486b530fc8b1bad133123c
polina <palinagolgaya@yandex.ru>
2024-03-26 13:46:20 +0300

    обработка команды start

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('7190036484:AAG1KC_QhMtZLDPopV3gW6ELKpvFlhrcvGo')
+
+hello_text = ''
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Привет!')
+    marcup = types.ReplyKeyboardMarkup()
+    marcup.add(types.KeyboardButton('В гостях у Бабы Нюры'))
+    marcup.add(types.KeyboardButton('В шашлычной у Ашота'))
+    marcup.add(types.KeyboardButton('Игры на выживание'))
+    bot.send_message(message.chat.id,
+                     f'Привет, {message.from_user.first_name}!\nО чем эти разделы: '
+                     f'\n🌸 В гостях у Бабы Нюры - раздел с советами о посадках '
+                     f'\n🥩 В шашлычной у Ашота - викторина о шашлыке '
+                     f'\n⚽️ Игры на выживание - идеи для отдыха',
+                     parse_mode='html', reply_markup=marcup)
 
 
 bot.polling(none_stop=True)
