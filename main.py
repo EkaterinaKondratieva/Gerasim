5a22626fb29ce4d0f2c3dd537bad93fdd8944286
polina <palinagolgaya@yandex.ru>
2024-04-17 18:52:57 +0300

    небольшие изменения там сям

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 import sqlite3
 import telebot
 from telebot import types
@@ -17,7 +16,6 @@ API_KEY = 'a7cd0d9a75754013bea6553cc27adc54'
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_photo(message.chat.id, photo=open(f'gerasim.jpeg', 'rb'))
-    # bot.send_message(message.chat.id, 'ХАЙ ЮЗЕР ВЫБИРАЙ РАЗДЕЛ')
     marcup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     marcup.add(types.KeyboardButton('В гостях у Бабы Нюры'))
     marcup.add(types.KeyboardButton('В шашлычной у Ашота'))
@@ -28,7 +26,6 @@ def start(message):
                      f'\n🥩 «В шашлычной у Ашота» - викторина о шашлыке для тех, кто в поиске идеального рецепта '
                      f'\n⚽️ «Игры на выживание» - идеи для отдыха: как занять всю компанию и не заскучать',
                      parse_mode='html', reply_markup=marcup)
-    bot.register_next_step_handler(message, on_click)
 
 
 @bot.message_handler(content_types=['text'])
@@ -39,12 +36,23 @@ def on_click(message):
         greeting(message)
     elif message.text.lower() == 'игры на выживание':
         games(message)
+    elif message.text.lower() == 'к начальному меню':
+        menu(message)
+
+
+def menu(message):
+    marcup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    marcup.add(types.KeyboardButton('В гостях у Бабы Нюры'))
+    marcup.add(types.KeyboardButton('В шашлычной у Ашота'))
+    marcup.add(types.KeyboardButton('Игры на выживание'))
+    bot.send_message(message.chat.id, 'С возвразщением', reply_markup=marcup)
 
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
     global about_seed
     if callback.data == 'return':
+        bot.edit_message_reply_markup(callback.message.chat.id, callback.message.message_id)
         marcup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
         marcup.add(types.KeyboardButton('В гостях у Бабы Нюры'))
         marcup.add(types.KeyboardButton('В шашлычной у Ашота'))
@@ -52,6 +60,7 @@ def callback_message(callback):
         bot.send_message(callback.message.chat.id, 'С возвразщением', reply_markup=marcup)
         bot.register_next_step_handler(callback.message, on_click)
     if callback.data == 'return_to_list':
+        bot.edit_message_reply_markup(callback.message.chat.id, callback.message.message_id)
         only_buttons(callback.message)
     elif callback.data in ['tomatoes', 'cucumbers', 'peppers', 'zucchini', 'carrot', 'strawberry']:
         bot.edit_message_reply_markup(callback.message.chat.id, callback.message.message_id)
@@ -61,20 +70,17 @@ def callback_message(callback):
         about_seed_temp = cur.execute('''SELECT Information FROM Seeds WHERE Name = ?''', (sort_of_seed,)).fetchone()[
             0].split('-')
         best_temp = [int(about_seed_temp[0]), int(about_seed_temp[-1])]
-        # СПРОСИТЬ ПОЧЕМУ ЗАДВАИВАЮТСЯ СООБЩЕНИЯ, ЕСЛИ МЕНЯТЬ ВЫБОР НЕ ДОЖИДАЯСЬ ОТВЕТА
         about_seed = []
         about_seed.append(callback.data)
         about_seed.append(sort_of_seed)
         about_seed.append(best_temp)
-
         marcup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        marcup.add(types.KeyboardButton("Отправить местоположение📍", request_location=True))
-        marcup.add(types.KeyboardButton('Посмотреть советы'))
-        marcup.add(types.KeyboardButton('Вернуться к списку'))
+        marcup.row(types.KeyboardButton("Отправить местоположение📍", request_location=True),
+                   types.KeyboardButton('Посмотреть советы'))
+        marcup.row(types.KeyboardButton('Вернуться к списку'), types.KeyboardButton('К начальному меню'))
         bot.send_message(callback.message.chat.id,
                          'Дружок, чтобы моя помощь была максимальной, мне нужно узнать твою геолокацию',
                          reply_markup=marcup)
-
         bot.register_next_step_handler(callback.message, help_nura)
 
 
@@ -86,7 +92,7 @@ def create_buttuns():
     btn4 = types.InlineKeyboardButton('Кабачки🤮', callback_data='zucchini')
     btn5 = types.InlineKeyboardButton('Морковь🥕', callback_data='carrot')
     btn6 = types.InlineKeyboardButton('Клубника🍓', callback_data='strawberry')
-    btn7 = types.InlineKeyboardButton('Назад', callback_data='return')
+    btn7 = types.InlineKeyboardButton('К начальному меню', callback_data='return')
     mupcup.row(btn1, btn2)
     mupcup.row(btn3, btn4)
     mupcup.row(btn5, btn6)
@@ -95,13 +101,23 @@ def create_buttuns():
 
 
 def only_buttons(message):
-    bot.send_message(message.chat.id, 'А вот и список',
-                     reply_markup=create_buttuns())
+    bot.send_message(message.chat.id, 'А вот и список', reply_markup=create_buttuns())
 
 
-@bot.message_handler(commands=['nura'])
+def answer(message):
+    bot.send_message(message.chat.id, 'Вот несколько рекомендаций для посадки:')
+    bot.send_photo(message.chat.id, photo=open(f'vegetables/{about_seed[0]}.jpeg', 'rb'))
+    mupcup = types.InlineKeyboardMarkup()
+    for i in range(len(SEEDS[about_seed[0]][1])):
+        mupcup.add(
+            types.InlineKeyboardButton(f'{SEEDS[about_seed[0]][1][i][0]}', url=SEEDS[about_seed[0]][1][i][1]))
+    mupcup.add(types.InlineKeyboardButton('К начальному меню', callback_data='return'))
+    mupcup.add(types.InlineKeyboardButton('Вернуться к списку', callback_data='return_to_list'))
+    bot.send_message(message.chat.id, 'А это мои лучшие семена)', reply_markup=mupcup)
+
+
 def start_nura(message):
-    bot.send_animation(message.chat.id, open('video/ogorod.mp4', 'rb'))
+    bot.send_animation(message.chat.id, open('video/ogorod.mp4', 'rb'), reply_markup=types.ReplyKeyboardRemove())
     bot.send_message(message.chat.id, 'Привет, внучок! \n'
                                       'Меня зовут Баба Нюра и я знаю все о помидорках и клубнике!\n'
                                       'Если тебе нужна моя помощь, то просто выбирай нужную культуру',
@@ -110,17 +126,11 @@ def start_nura(message):
 
 def help_nura(message):
     if message.text == 'Посмотреть советы':
-        bot.send_photo(message.chat.id, photo=open(f'vegetables/{about_seed[0]}.jpeg', 'rb'))
-        mupcup = types.InlineKeyboardMarkup()
-        for i in range(len(SEEDS[about_seed[0]][1])):
-            mupcup.add(
-                types.InlineKeyboardButton(f'{SEEDS[about_seed[0]][1][i][0]}', url=SEEDS[about_seed[0]][1][i][1]))
-        mupcup.add(types.InlineKeyboardButton('Назад', callback_data='return'))
-        mupcup.add(types.InlineKeyboardButton('Вернуться к списку', callback_data='return_to_list'))
-        bot.send_message(message.chat.id, 'А это мои лучшие семена)', reply_markup=mupcup)
+        answer(message)
     elif message.text == 'Вернуться к списку':
         only_buttons(message)
-
+    elif message.text == 'К начальному меню':
+        menu(message)
     else:
         lat = message.location.latitude
         lon = message.location.longitude
@@ -139,30 +149,22 @@ def help_nura(message):
             now_temp /= 5
             if about_seed[-1][0] <= now_temp <= about_seed[-1][-1]:
                 bot.send_message(message.chat.id,
-                                 f'{now_temp} - хорошая погода, чтобы посадить {seed}')
+                                 f'<b>{now_temp} - хорошая погода, чтобы посадить {seed}</b>', parse_mode='html')
             else:
-                bot.send_message(message.chat.id, f'Средняя температура на неделе - {now_temp}, недостаточно тепло.'
-                                                  f'Нужно подождать, чтобы посадить {seed}')
+                bot.send_message(message.chat.id, f'<b>Средняя температура на неделе - {now_temp}, недостаточно тепло.'
+                                                  f'Нужно подождать, чтобы посадить {seed}</b>', parse_mode='html')
         else:
-            bot.send_message(message.chat.id, f'На неделе ожидается низкая температура,'
-                                              f' нужно подождать, чтобы посадить {seed}')
-        bot.send_message(message.chat.id, 'Вот несколько рекомендаций для посадки:')
-        bot.send_photo(message.chat.id, photo=open(f'vegetables/{about_seed[0]}.jpeg', 'rb'))
-        mupcup = types.InlineKeyboardMarkup()
-        for i in range(len(SEEDS[about_seed[0]][1])):
-            mupcup.add(
-                types.InlineKeyboardButton(f'{SEEDS[about_seed[0]][1][i][0]}', url=SEEDS[about_seed[0]][1][i][1]))
-        mupcup.add(types.InlineKeyboardButton('Назад', callback_data='return'))
-        mupcup.add(types.InlineKeyboardButton('Вернуться к списку', callback_data='return_to_list'))
-        bot.send_message(message.chat.id, 'А это мои лучшие семена)', reply_markup=mupcup)
+            bot.send_message(message.chat.id, f'<b>На неделе ожидается низкая температура,'
+                                              f' нужно подождать, чтобы посадить {seed}</b>', parse_mode='html')
+        answer(message)
 
 
 ##ASHOT
 @bot.message_handler(commands=['ashot'])
 def greeting(message):
     mupcup = types.InlineKeyboardMarkup()
-    mupcup.add(types.InlineKeyboardButton('Назад', callback_data='return'))
-    bot.send_animation(message.chat.id, open('video/meat.mp4', 'rb'))
+    mupcup.add(types.InlineKeyboardButton('К начальному меню', callback_data='return'))
+    bot.send_animation(message.chat.id, open('video/meat.mp4', 'rb'), reply_markup=types.ReplyKeyboardRemove())
     bot.send_message(message.chat.id, 'Вай, кого я вижу! \n'
                                       'Мой сладкий пирожок захотел познать искусство'
                                       ' приготовления идеального шашлыка? Давай посмотрим, сможешь ли ты создать такой'
@@ -173,8 +175,10 @@ def greeting(message):
 @bot.message_handler(commands=['games'])
 def games(message):
     mupcup = types.InlineKeyboardMarkup()
-    mupcup.add(types.InlineKeyboardButton('Назад', callback_data='return'))
+    mupcup.add(types.InlineKeyboardButton('К начальному меню', callback_data='return'))
     bot.send_animation(message.chat.id, open('video/game.mp4', 'rb'), reply_markup=mupcup)
+    bot.send_message(message.chat.id, 'удаляем нижние кнопки, потом заменим на приветственный текст',
+                     reply_markup=types.ReplyKeyboardRemove())
 
 
 bot.polling(none_stop=True)
