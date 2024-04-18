6d9650089e8900e076daa42fc5d5d1f2c47b3540
Ekaterina <edkon06@gmail.com>
2024-04-18 15:06:50 +0300

    делала это весь день, короче весь ашотик и приколюхи. Полина неграмоная

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import telebot
 from telebot import types
 import pymorphy2
 import requests
+
 from seeds import SEEDS
 
 bot = telebot.TeleBot('7190036484:AAG1KC_QhMtZLDPopV3gW6ELKpvFlhrcvGo')
@@ -51,15 +52,16 @@ def menu(message):
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
     global about_seed
+    global k
     bot.edit_message_reply_markup(callback.message.chat.id, callback.message.message_id)
     if callback.data == 'return':
         marcup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
         marcup.add(types.KeyboardButton('В гостях у Бабы Нюры'))
         marcup.add(types.KeyboardButton('В шашлычной у Ашота'))
         marcup.add(types.KeyboardButton('Игры на выживание'))
-        bot.send_message(callback.message.chat.id, 'С возвразщением', reply_markup=marcup)
+        bot.send_message(callback.message.chat.id, 'С возвращением', reply_markup=marcup)
         bot.register_next_step_handler(callback.message, on_click)
-    if callback.data == 'return_to_list':
+    elif callback.data == 'return_to_list':
         only_buttons(callback.message)
     elif callback.data in ['tomatoes', 'cucumbers', 'peppers', 'zucchini', 'carrot', 'strawberry']:
         con = sqlite3.connect('bd.sql')
@@ -81,7 +83,7 @@ def callback_message(callback):
                          reply_markup=marcup)
         bot.register_next_step_handler(callback.message, help_nura)
     elif callback.data == 'return_to_games':
-        bot.send_message(callback.message.chat.id, 'С возвращением', reply_markup=create_buttuns_for_game())
+        bot.send_message(callback.message.chat.id, 'С возвращением', reply_markup=create_buttons_for_game())
     elif callback.data == 'two' or callback.data == 'three and more' or callback.data == 'big company':
         if callback.data == 'two':
             n = 2
@@ -105,9 +107,17 @@ def callback_message(callback):
         for i in range(len(games_in_bd)):
             text += f'\n{emoji} {games_in_bd[i][0]}'
         bot.send_message(callback.message.chat.id, text, reply_markup=marcup)
+    elif callback.data == 'lets go':
+        k = 0
+        bot.send_message(callback.message.chat.id, '', reply_markup=shashlik(callback.message, k))
+    elif callback.data == 'return no message':
+        bot.send_message(callback.message.chat.id, '', reply_markup=shashlik(callback.message, k))
+    elif callback.data == 'forward':
+        k += 1
+        bot.send_message(callback.message.chat.id, '', reply_markup=shashlik(callback.message, k))
 
 
-def create_buttuns_for_game():
+def create_buttons_for_game():
     mupcup = types.InlineKeyboardMarkup()
     mupcup.add(types.InlineKeyboardButton('2 человека', callback_data='two'))
     mupcup.add(types.InlineKeyboardButton('3-6 человек', callback_data='three and more'))
@@ -116,7 +126,7 @@ def create_buttuns_for_game():
     return mupcup
 
 
-def create_buttuns_for_nura():
+def create_buttons_for_nura():
     mupcup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton('Помидоры🍅', callback_data='tomatoes')
     btn2 = types.InlineKeyboardButton('Огурцы🥒', callback_data='cucumbers')
@@ -133,7 +143,7 @@ def create_buttuns_for_nura():
 
 
 def only_buttons(message):
-    bot.send_message(message.chat.id, 'А вот и список', reply_markup=create_buttuns_for_nura())
+    bot.send_message(message.chat.id, 'А вот и список', reply_markup=create_buttons_for_nura())
 
 
 def answer(message):
@@ -153,7 +163,7 @@ def start_nura(message):
     bot.send_message(message.chat.id, 'Привет, внучок! \n'
                                       'Меня зовут Баба Нюра и я знаю все о помидорках и клубнике!\n'
                                       'Если тебе нужна моя помощь, то просто выбирай нужную культуру',
-                     reply_markup=create_buttuns_for_nura())
+                     reply_markup=create_buttons_for_nura())
 
 
 def help_nura(message):
@@ -195,13 +205,59 @@ def help_nura(message):
 @bot.message_handler(commands=['ashot'])
 def greeting(message):
     mupcup = types.InlineKeyboardMarkup()
+    mupcup.add(types.InlineKeyboardButton('Поехали', callback_data='lets go'))
     mupcup.add(types.InlineKeyboardButton('К начальному меню', callback_data='return'))
     bot.send_animation(message.chat.id, open('video/meat.mp4', 'rb'), reply_markup=types.ReplyKeyboardRemove())
     bot.send_message(message.chat.id, 'Вай, кого я вижу! \n'
                                       'Мой сладкий пирожок захотел познать искусство'
                                       ' приготовления идеального шашлыка? Давай посмотрим, сможешь ли ты создать такой'
-                                      ' шедевр, поедая который гости не заметят, что съели собственные усы 🤌',
+                                      ' шедевр, поедая который гости не заметят, что съели собственные усы)',
                      reply_markup=mupcup)
+
+
+def shashlik(message, k):
+    mupcup = types.InlineKeyboardMarkup()
+    mupcup.add(types.InlineKeyboardButton('Конечно, Ашотик', callback_data='forward'))
+    mupcup.add(types.InlineKeyboardButton('К начальному меню', callback_data='return no message'))
+    if k == 0:
+        bot.send_message(message.chat.id, 'Ради таких смелых людей сбрил бы бороду!')
+        bot.send_poll(message.chat.id, 'Начну с простого: из какого мяса традиционно делается шашлык?',
+                      ['Свинина', 'Говядина', 'Курица', 'Баранина'], None, 'quiz', None, 3,
+                      'Традиционный шашлык — это еда кочевников Азии. Он изготавливался из баранины,'
+                      ' которая жарилась на металлических или деревянных прутьях над огнем')
+
+        bot.send_message(message.chat.id, 'Ну что, продолжаем познавать азы готовки?', reply_markup=mupcup)
+    elif k == 1:
+        bot.send_poll(message.chat.id,
+                      'У хорошего хозяина шашлыка хватает всегда и на всех! По сколько грамм на человека'
+                      'принято покупать мяса?',
+                      ['100-200 грамм', '200-300 грамм', '300-400 грамм'], None, 'quiz', None, 2,
+                      'Мясо обычно берут из расчета 300-400 г на каждого горячо любимого гостя'
+                      '. Больше – можно, меньше – не стоит\n'
+                      'В процессе приготовления этот продукт обязательно потеряет в весе')
+        bot.send_message(message.chat.id, 'Был бы ты воробьём, тебя бы звали Орёл! Идём дальше?', reply_markup=mupcup)
+    elif k == 2:
+        bot.send_poll(message.chat.id, 'Уточним технические моменты: '
+                                       'какие шампуры лучше использовать для "шашлычного" мяса?',
+                      ['Плоские', 'Угловые'], None, 'quiz', None, 0,
+                      'ПЛОСКИЕ шампуры - универсальные: подойдут и для приготовления люля кебаб(он не сползет и'
+                      ' будет хорошо держаться, больших кусков мяса\n'
+                      'УГЛОВЫЕ подойдут для жарки грибов и небольших кусков мяса')
+        bot.send_message(message.chat.id, 'Пойду покажу своей маме, какой волк проходит мой тест. Вперёд?',
+                         reply_markup=mupcup)
+    elif k == 3:
+        bot.send_poll(message.chat.id, 'Шампуры помыты, мясо куплено. Как следует его нарезать?',
+                      ['Вдоль мышечных волокон', 'Поперёк мышечных волокон'], None, 'quiz', None, 1,
+                      ' Делая это поперёк волокон, а не вдоль, ты укорачиваешь их, мой золотой.'
+                      ' Мясо станет мягче, его будет легко прожевать.')
+
+        bot.send_message(message.chat.id, 'Вайя, какое рвение к победе! Заводим мою ниву и едем дальше?',
+                         reply_markup=mupcup)
+    elif k == 5:
+        bot.send_message(message.chat.id, 'Это конец, моя маковая росинка! До встречи в моей шашлычной, пусть все твои '
+                                          'шашлыки будут вкуснее, чем у соседей!')
+
+    # GAMES
 
 
 def games(message):
